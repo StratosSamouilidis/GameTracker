ca6e33aff8f3f2f03e8f9779807b94ba5ee4c932
Κωνσταντινος Αβραμης <K.Avramis@hotmail.com>
2018-10-20 17:39:14 +0300

    Web Request Test1

--- GameTracker/GameTracker/Form1.cs
+++ GameTracker/GameTracker/Form1.cs
@@ -16,5 +16,13 @@ namespace GameTracker
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PageCall pc = new PageCall();
+            pc.DownloadPageHttpWebRequest();
+            
+           
+        }
     }
 }
---
+++ GameTracker/GameTracker/PageCall.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+
+namespace GameTracker
+{
+    class PageCall
+    {
+        private string html { get; set; }
+        private string url { get; set; }
+
+        public PageCall() { }
+        public PageCall(string html,string url)
+        {
+            html = this.html;
+            url = this.url;
+        }
+        
+       public void DownloadPageHttpWebRequest()
+        {
+             html = string.Empty;
+             url = "https://www.youtube.com";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream)) {
+
+                html = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(html);
+        }
+
+    }
+}
