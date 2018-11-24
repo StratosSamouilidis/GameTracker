e129b28a8c8cf9614af8b0a1a831cfbb98b6f74b
Κωνσταντινος Αβραμης <K.Avramis@hotmail.com>
2018-11-24 16:17:20 +0200

    Browser N DB

--- GameTracker/GameTracker/LoadResults.cs
+++ GameTracker/GameTracker/LoadResults.cs
@@ -159,7 +159,7 @@ namespace GameTracker
             }
             else
             {
-                HtmlResultsBox.Text += "No Result where found kappa ";
+                HtmlResultsBox.Text += "No Result where found  ";
             }
 
         }
